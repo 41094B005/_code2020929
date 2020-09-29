a3680acd5bd35036a82bc9c4db3945877627d4cc
41094B005 <71592891+41094B005@users.noreply.github.com>
2020-09-29 23:00:28 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,71 @@
+let Gpa = 0
+input.onPinPressed(TouchPin.P0, function () {
+    Gpa = randint(80, 100)
+    basic.showNumber(Gpa)
+    if (Gpa == 80) {
+        basic.showString("A-")
+    }
+    if (Gpa == 81) {
+        basic.showString("A-")
+    }
+    if (Gpa == 82) {
+        basic.showString("A-")
+    }
+    if (Gpa == 83) {
+        basic.showString("A-")
+    }
+    if (Gpa == 84) {
+        basic.showString("A-")
+    }
+    if (Gpa == 85) {
+        basic.showString("A")
+    }
+    if (Gpa == 86) {
+        basic.showString("A")
+    }
+    if (Gpa == 87) {
+        basic.showString("A")
+    }
+    if (Gpa == 88) {
+        basic.showString("A")
+    }
+    if (Gpa == 89) {
+        basic.showString("A")
+    }
+    if (Gpa == 90) {
+        basic.showString("A+")
+    }
+    if (Gpa == 91) {
+        basic.showString("A+")
+    }
+    if (Gpa == 92) {
+        basic.showString("A+")
+    }
+    if (Gpa == 93) {
+        basic.showString("A+")
+    }
+    if (Gpa == 94) {
+        basic.showString("A+")
+    }
+    if (Gpa == 95) {
+        basic.showString("A+")
+    }
+    if (Gpa == 96) {
+        basic.showString("A+")
+    }
+    if (Gpa == 97) {
+        basic.showString("A+")
+    }
+    if (Gpa == 98) {
+        basic.showString("A+")
+    }
+    if (Gpa == 99) {
+        basic.showString("A+")
+    }
+    if (Gpa == 100) {
+        basic.showString("A+")
+    }
+})
+basic.forever(function () {
+	
+})
